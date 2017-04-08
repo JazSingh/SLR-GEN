360995eefe8c81e37fe4f6d1036720414ca89e03
Jaspreet Singh <j.singh@students.uu.nl>
2017-04-08 21:21:07 +0200

    first version

--- js/script.js
+++ js/script.js
@@ -2,11 +2,31 @@ $(function() {
     console.log( "ready!" );
 
     $( "#generate-button" ).click(function() {
-        var potato = $(".term-group");
-        console.log(potato);
+        var $termGroups = $(".term-group");
+
+        var groups = [];
+        var j = 0;
+        for(var i = 0; i < $termGroups.size(); i++) {
+            var terms = $(":input", $termGroups[i]).map(function() {
+                return $(this).val() === "" ? null : "\"" + $(this).val() + "\"";
+            });
+
+            var termArr = $.makeArray(terms);
+            if(checkNotEmpty(termArr)) {
+                groups[j]= "(" + termArr.join(" OR ") + ")";
+                j++;
+            }
+        }
+
+        var query = "TABLE EMPTY";
+        if(checkNotEmpty(groups)) {
+            query = "(" + groups.join(" AND ") + ")";
+        }
+        $("#result").text(query);
     });
 
-    function getValues(group) {
-
+    function checkNotEmpty(arrayObj) {
+        return arrayObj.length > 0
     }
+
 });
