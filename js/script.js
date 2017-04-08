d804082fab11e7747ce0862948f64cf5284015f9
Jaspreet Singh <j.singh@students.uu.nl>
2017-04-08 15:31:23 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    console.log( "ready!" );
+
+    $( "#generate-button" ).click(function() {
+        var potato = $(".term-group");
+        console.log(potato);
+    });
+
+    function getValues(group) {
+
+    }
+});
